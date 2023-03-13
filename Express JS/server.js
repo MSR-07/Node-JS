7f44906d1df2d10e641c24849e172e3dba93d164
MSR-07 <102695412+MSR-07@users.noreply.github.com>
2023-03-13 19:01:28 +0500

    Add param req

--- Express JS/server.js
+++ Express JS/server.js
@@ -2,9 +2,35 @@ const express = require("express");
 const app = express();
 
 const PORT = 3000;
+const friends = [
+  {
+    id: 0,
+    name: "Umer Bhi",
+  },
+  {
+    id: 1,
+    name: "Hamza Bhi",
+  },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello , Buddy..............!");
+});
+
+app.get("/friend", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friend/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exit.",
+    });
+  }
 });
 
 app.get("/message", (req, res) => {
