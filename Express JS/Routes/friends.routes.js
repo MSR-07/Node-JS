a0637945ced5bf88ce136af1c3220288b1c8b28e
MSR-07 <102695412+MSR-07@users.noreply.github.com>
2023-03-14 15:18:39 +0500

    Add IP in friend route

--- Express JS/Routes/friends.routes.js
+++ Express JS/Routes/friends.routes.js
@@ -2,6 +2,11 @@ const express = require("express");
 const friendRouter = express.Router();
 const friendsControllers = require("../Controllers/friends.controller");
 
+friendRouter.use((req, res, next) => {
+  console.log("ip address:", req.ip);
+  next();
+});
+
 friendRouter.get("/", friendsControllers.getFriends);
 friendRouter.get("/:friendId", friendsControllers.getFriendById);
 friendRouter.post("/", friendsControllers.postFriend);
