45148516cc75e0d1ecf3bd629eb0c11d52c87cf2
MSR-07 <102695412+MSR-07@users.noreply.github.com>
2023-03-14 11:37:34 +0500

    Change in post req

--- Express JS/server.js
+++ Express JS/server.js
@@ -20,6 +20,24 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} ${delta}ms`);
 });
 
+app.use(express.json());
+
+app.post("/friend", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: "Missing Friend Name.",
+    });
+  } else {
+    const newFriend = {
+      name: req.body.name,
+      id: friends.length,
+    };
+    friends.push(newFriend);
+
+    res.json(newFriend);
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello , Buddy..............!");
 });
